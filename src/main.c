a94e7c642f1376070306dbd92371a274015e7e61
Chase Nickel <nickel5@outlook.com>
2024-05-08 21:01:11 -0500

    I did your work for you loser

--- src/main.c
+++ src/main.c
@@ -3,31 +3,56 @@
 // TODO: invoke linkerwith resultant object files
 //
 
-
 #include <stdio.h>
 
-extern int ncc(char*);
+extern int ncc(char *);
 
 void help();
 
-int main(int argc, char** argv) {
+#include <lexer.h>
+#include <token.h>
 
-	if (argc == 1) {
-		help();
-		return 0;
-	}
-	
-	for (int i=1; i<argc; i++) {
-		if(!ncc(argv[i])) {
-			puts("Compilation failed.");
-			return -1;
-		}
-	}
-
-	return 0;
+void print_tok(Token t) {
+    switch (t.type) {
+    case SYMBOL:
+    case NUMBER:
+        printf("<%.*s> %zu\n", (int)t.len, t.start, t.len);
+        return;
+    case ILLEGAL:
+        puts("ILLEGAL");
+        return;
+    case END_OF_FILE:
+        puts("EOF");
+        return;
+    default:
+        printf("<%c>\n", *t.start);
+        return;
+    }
 }
 
-void help() {
-	puts("I\'m helping");
+int main(int argc, char **argv) {
+
+    // if (argc == 1) {
+    // 	help();
+    // 	return 0;
+    // }
+    //
+    // for (int i=1; i<argc; i++) {
+    // 	if(!ncc(argv[i])) {
+    // 		puts("Compilation failed.");
+    // 		return -1;
+    // 	}
+    // }
+
+    const char expr[] = "apple + bananana- cheese";
+    Lexer l = new_lexer(expr, sizeof expr - 1);
+    Token t = {0};
+    while (t.type != END_OF_FILE) {
+        t = lex_next_token(&l);
+        print_tok(t);
+    }
+
+    return 0;
 }
 
+void help() { puts("I\'m helping"); }
--- src/ncc.c
+++ src/ncc.c
@@ -3,68 +3,69 @@
 // TODO: implement LEXING, PARSING, SEMANTIC ANALYSIS, OPTIMIZATION, GENERATION
 //
 
+extern int preprocessor(char *, long, char *);
 
-extern int preprocessor(char*, long, char*);
-
+#include <errno.h>
+#include <error.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <errno.h>
 #include <string.h>
-#include <error.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-int ncc(char* filename) {
+int ncc(char *filename) {
 
-	// Open the file
-	int fd = open(filename, O_RDWR);
-	if (fd == -1) {
-		ncc_error("ncc: ", "Failed to open file.");
-		return 0;
-	}
+    // Open the file
+    int fd = open(filename, O_RDWR);
+    if (fd == -1) {
+        ncc_error("ncc: ", "Failed to open file.");
+        return 0;
+    }
 
-	// Create the .pp preprocessed file's name
-	int filename_len = strlen(filename);
-	char* output_filename = malloc(filename_len);
-	memcpy(output_filename, filename, filename_len);
-	output_filename[filename_len-1] = 'p';
-	output_filename[filename_len-2] = 'p';
+    // Create the .pp preprocessed file's name
+    int filename_len = strlen(filename);
+    char *output_filename = malloc(filename_len);
+    memcpy(output_filename, filename, filename_len);
+    output_filename[filename_len - 1] = 'p';
+    output_filename[filename_len - 2] = 'p';
 
-	// Get file length
-	struct stat st;
-	fstat(fd, &st);
-	long filelen = st.st_size;
-	
-	// Call mmap
-	char* filedata = mmap(0, filelen, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	if (filedata == (void*)-1) {
-		ncc_error("ncc: ", "Failed to map memory.");
-		switch (errno) {
-			case EACCES:
-				puts("EACCES");
-				break;
-			case EBADF:
-				puts("Bad FD");
-				break;
-			case EINVAL:
-				puts("EINVAL");
-				break;
-			default:
-				puts("Other error");
-				break;
-		}
-		return 0;
-	}
-	close(fd);
-	
-	// Invoke the preprocessor
-	if(!preprocessor(filedata, filelen, output_filename)) {
-		puts("Preprocessor error. Terminating.");
-		return 0;
-	}
+    // Get file length
+    struct stat st;
+    fstat(fd, &st);
+    long filelen = st.st_size;
 
-	// Unmap & return
-	munmap(filedata, filelen);
-	return 1;
+    // Call mmap
+    char *filedata =
+        mmap(0, filelen, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (filedata == (void *)-1) {
+        ncc_error("ncc: ", "Failed to map memory.");
+        switch (errno) {
+        case EACCES:
+            puts("EACCES");
+            break;
+        case EBADF:
+            puts("Bad FD");
+            break;
+        case EINVAL:
+            puts("EINVAL");
+            break;
+        default:
+            puts("Other error");
+            break;
+        }
+        return 0;
+    }
+    close(fd);
+
+    // Invoke the preprocessor
+    if (!preprocessor(filedata, filelen, output_filename)) {
+        puts("Preprocessor error. Terminating.");
+        return 0;
+    }
+
+    // Unmap & return
+    munmap(filedata, filelen);
+    return 1;
 }
-
